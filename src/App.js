1ba56c95e63a41f4fddee9d31e33c99157eb3b1b
arisudarma <arisudarma@gmail.com>
2021-11-25 18:05:38 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from "./component/Header";
+import Tasks from "./component/Tasks";
+import AddTask from "./component/AddTask";
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Learn react',
+      day: 'Feb 6th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Learn Vue',
+      day: 'Feb 7th at 2:30pm',
+      reminder: false
+    }
+  ])
+
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //set reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = {...task, id}
+    setTasks([...tasks, newTask])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask ? (<AddTask onAdd={addTask}/>) : ''}
+      { tasks.length > 0 ?
+        (<Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+          />
+        )
+        :
+        ('No Tasks To Show')
+      }
     </div>
   );
 }
---
+++ src/component/Tasks.js
@@ -0,0 +1,20 @@
+import Task from "./Task";
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <div>
+      {
+        tasks.map((task) => (
+          <Task
+            task={task}
+            key={task.id}
+            onDelete={onDelete}
+            onToggle={onToggle}
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+export default Tasks
